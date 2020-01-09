1e79deba636a9ea2944c77db65dc2ee20d789592
lhg1995 <1209114452@qq.com>
2020-01-09 15:21:50 +0800

    异步测试

--- fatchData.test.js
+++ fatchData.test.js
@@ -1,9 +1,18 @@
 import {fetchData} from './fatchData';
-test('fetchData 返回结果为{success: true}',(done)=>{//测试异步要用done
-    fetchData((data)=>{
-        expect(data).toEqual({
-            success:true
+// 回调类型异步函数测试
+// test('fetchData 返回结果为{success: true}',(done)=>{//测试异步要用done
+//     fetchData((data)=>{
+//         expect(data).toEqual({
+//             success:true
+//         })
+//         done();
+//     })
+// })
+
+test('fetchData 返回结果为{success: true}',()=>{
+    fetchData().then((response)=>{
+        expect(response.data).toEqual({
+            success: true
         })
-        done();
     })
 })
